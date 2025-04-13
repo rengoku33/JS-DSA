79001c40bded773031a7ebaef41e9212e2251b3d
Kiran Vignesh <rajkiranvignesh369@gmail.com>
2025-04-13 13:22:02 +0530

    TOKIOOO

--- array, set, map, string, objects.js
+++ array, set, map, string, objects.js
@@ -15,5 +15,13 @@
   // create array of arrays
       let arr = new Array(size).fill().map(() => []);
 
+  // create a 2D array
+      let visited = Array.from({length: board.length}, () => 
+		    new Array(board[0].length).fill(false)
+	    )	
+
+  // add an array to an array
+      outerArr.push([...innerArr]);
+
   // compare two arrays
       JSON.stringify(arr1) == JSON.stringify(arr2)
