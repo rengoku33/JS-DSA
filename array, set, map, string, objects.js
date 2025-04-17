21b75fa624b72c18976f1e2e7eb739dd81e84d70
Kiran Vignesh <rajkiranvignesh369@gmail.com>
2025-04-17 13:41:37 +0530

    added mod and dupe check

--- array, set, map, string, objects.js
+++ array, set, map, string, objects.js
@@ -1,30 +1,33 @@
 // Basic ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-  // Ascii conversion
-  a = 97, A = 65
-      'char'.charCodeAt(0) - 'a';
+   // Ascii conversion
+	a = 97, A = 65
+      	'char'.charCodeAt(0) - 'a';
+
+   // "%" - modulus operator returns remainder,   "/" - returns quotient
+	10%2 == 0 /* true */  10/2 /* 5 */
 
 // SET ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-  // array to set and vice verca: 
-      let sett = new Set(arr);
-      let arr = [...sett];
+   // array to set and vice verca: 
+      	let sett = new Set(arr);
+      	let arr = [...sett];
 
 // Array ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-  // create new array of default size and fill it with empty values (initially "empty items" not "undefined" and this is not preferred)
-      let arr = new Array(26).fill(0);
+   // create new array of default size and fill it with empty values (initially "empty items" not "undefined" and this is not preferred)
+      	let arr = new Array(26).fill(0);
 
-  // create "empty" array of arrays
-      let arr = new Array(size).fill().map(() => []);
+   // create "empty" array of arrays
+      	let arr = new Array(size).fill().map(() => []);
 
-  // create a 2D array
-      let visited = Array.from({length: board.length}, () => 
+   // create a 2D array
+      	let visited = Array.from({length: board.length}, () => 
 		    new Array(board[0].length).fill(false)
 	    )	
 
-  // add an array to an array
-      outerArr.push([...innerArr]);
+   // add an array to an array
+      	outerArr.push([...innerArr]);
 
-  // compare two arrays
-      JSON.stringify(arr1) == JSON.stringify(arr2)
+   // compare two arrays
+      	JSON.stringify(arr1) == JSON.stringify(arr2)
 
    // array to string:
 	let str = arr.join('') // by default it will join my "," if argument is empty, you can customize it eg: '' will simply merge all, '.' will merge with "." between elements
@@ -34,3 +37,14 @@
 	let charArray = str.split('');
 	//or 
 	let charArray = [...str];
+
+
+
+
+
+
+// ----------------------------------------------------------------------------------------------- PPACTICES ------------------------------------------------------------------------------------------------------------
+   // skip dupelicates ( used for permutation / subsets )
+	nums.sort((a, b) => a - b); // ✅ sort to handle duplicates (initially)
+	// inside dfs() for loop
+	if (i > idx && nums[i] === nums[i - 1]) continue; // ✅ skip duplicates at same depth
